17537fb413005f18f8d434490015924f0a448839
Natan Deitch <natan.luz.deitch@gmail.com>
2020-08-20 20:15:31 -0300

    feat: web app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,24 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { PassportModule } from '@nestjs/passport';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AuthnGuard } from './authn/authn.guard';
 import { AuthnResolver } from './authn/authn.resolver';
 import { AuthnStrategy } from './authn/authn.strategy';
 import { AuthzGuard } from './authz/authz.guard';
 import { AuthzResolver } from './authz/authz.resolver';
-import { PublicResolver } from './public/public.resolver';
 import { AuthzStrategy } from './authz/authz.strategy';
+import { PublicResolver } from './public/public.resolver';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
       context: ({ req }) => ({ req }),
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
     }),
     PassportModule
   ],
