1116431862ce735cfa60eb020be89f66be6beb6a
Natan Deitch <natan.luz.deitch@gmail.com>
2020-08-25 06:39:01 -0300

    feat: resource id on decorators

--- src/authz/authz.resolver.ts
+++ src/authz/authz.resolver.ts
@@ -1,27 +1,28 @@
-import { Args, Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { Scoped } from "./scoped.decorator";
 
 @Resolver(() => String)
 export class AuthzResolver {
-  @Scoped()
-  @Query(() => String)
-  createContent(): string {
-    return 'Hello from createContent'
+  @Scoped({ resourceId: 'folder' })
+  @Mutation(() => Boolean)
+  createContent(@Args('folder', { type: () => String }) folder: string): boolean {
+    console.log(`You've scope=${folder}:createContent`)
+    return true
   }
 
-  @Scoped({ resourceId: 'id' })
+  @Scoped()
   @Query(() => String)
   deleteContent(@Args('id', { type: () => String }) id: string): string {
     return `You've scope=${id}:deleteContent`
   }
 
-  @Scoped({ resourceId: 'id' })
+  @Scoped()
   @Query(() => String)
   getContent(@Args('id', { type: () => String }) id: string): string {
     return `You've scope=${id}:getContent`
   }
 
-  @Scoped({ resourceId: 'id' })
+  @Scoped()
   @Query(() => String)
   updateContentTitle(@Args('id', { type: () => String }) id: string): string {
     return `You've scope=${id}:updateContentTitle`
--- src/authz/scoped.decorator.ts
+++ src/authz/scoped.decorator.ts
@@ -1,7 +1,7 @@
 import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
 import { AuthzGuard } from './authz.guard';
 
-export function Scoped(resourceInfo?: { resourceId: string }): any {
+export function Scoped(resourceInfo: { resourceId: string } = { resourceId: 'id' }): any {
   return applyDecorators(
     SetMetadata('scoped', true),
     SetMetadata('resourceId', resourceInfo?.resourceId),
